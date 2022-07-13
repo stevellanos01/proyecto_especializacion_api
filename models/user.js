c8675b7db3537ef85500bb176a04283fc7bfe5e3
stevellanos01 <estebanllanos97@gmail.com>
2022-07-13 17:25:40 -0400

    Se agrega modelo user

--- models/user.js
+++ models/user.js
@@ -2,14 +2,21 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   id: {
-    type: Number,
-    required: true,
-  },
+    type: Number
+  },  
   username: {
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+  },
   password: {
+    type: String,
+    required: true,
+  },
+  role: {
     type: String,
     required: true,
   },
